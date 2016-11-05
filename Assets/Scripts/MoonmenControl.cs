95fcf88458cfaf94b827a5fb3cd7bb10c658916e
Rosalind Chang <rosalindjchang@gmail.com>
2016-11-05 10:39:34 -0700

    starting moon men script // not working properly yet though

--- Assets/Scripts/MoonmenControl.cs
+++ Assets/Scripts/MoonmenControl.cs
@@ -3,29 +3,18 @@ using System.Collections;
 
 public class MoonmenControl : MonoBehaviour {
 
-	float speed;
-	float originalspeed;
+	Transform player;
+	NavMeshAgent nav;
 
 
-	void Start () {
-		originalspeed = 0.01f;
-		speed = originalspeed;
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
 
+		nav = GetComponent<NavMeshAgent> ();
 	}
 
 	void Update () {
-
-		// get the monster's current position
-		Vector3 position = transform.position;
-
-		//compute the monster's new position
-		position = new Vector3 (position.x,position.y, position.z + speed + Time.deltaTime);
-
-		//update enemy position
-		transform.position = position;
-
-
-
+		nav.SetDestination (player.position);
 	}
 
 
