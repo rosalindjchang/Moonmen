fa70e0972ef6f0bd0e1616b96554bda1ebc3f537
Miller Klitsner <mklitsner@gmail.com>
2016-11-06 14:49:41 -0800

    Implementing moonmen fade. Not finished yet

--- Assets/Scripts/MaterialFade.cs
+++ Assets/Scripts/MaterialFade.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Test : MonoBehaviour 
+public class MaterialFade : MonoBehaviour 
 {
 	[Range (0.1f, 1.0f)]
 	public float fadeSpeed = 1f;    // How fast alpha value decreases.
 	private Color fadeColor = new Color (0, 0, 0, 0);
 
-	private Material m_Material;    // Used to store material reference.
-	private Color m_Color;            // Used to store color reference.
+	private Material m_Material0;   // Used to store material reference.
+	private Material m_Material1; 
+	private Color m_Color0;            // Used to store color reference.
+	private Color m_Color1;  
 
 
 	void Start ()
 	{
 		// Get reference to object's material.
-		m_Material = GetComponent <Renderer> ().material;
+		m_Material0 = GetComponent <SkinnedMeshRenderer> ().materials[0];
+		m_Material1 = GetComponent <SkinnedMeshRenderer> ().materials[1];
 
 		// Get material's starting color value.
-		m_Color = m_Material.color;
+		m_Color0 = m_Material0.color;
+		m_Color1 = m_Material1.color;
 
 		// Must use "StartCoroutine()" to execute 
 		// methods with return type of "IEnumerator".
@@ -39,7 +43,8 @@ public class Test : MonoBehaviour
 			// Create a new color using original color RGB values combined
 			// with new alpha value. We have to do this because we can't 
 			// change the alpha value of the original color directly.
-			m_Material.color = new Color (m_Color.r, m_Color.g, m_Color.b, alpha);
+			m_Material0.color = new Color (m_Color0.r, m_Color0.g, m_Color0.b, alpha);
+			m_Material1.color = new Color (m_Color1.r, m_Color1.g, m_Color1.b, alpha);
 
 			yield return null;
 		}
@@ -58,7 +63,8 @@ public class Test : MonoBehaviour
 			// Reduce change value by fadeSpeed amount.
 			change += fadeSpeed * Time.deltaTime;
 
-			m_Material.color = Color.Lerp (m_Color, fadeColor, change);
+			m_Material0.color = Color.Lerp (m_Color0, fadeColor, change);
+			m_Material1.color = Color.Lerp (m_Color1, fadeColor, change);
 
 			yield return null;
 		}
