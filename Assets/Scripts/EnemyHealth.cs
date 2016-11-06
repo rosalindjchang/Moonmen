5e2a722bcb54bcbdd633f03092f3f0e2b4e50cb1
Rosalind Chang <rosalindjchang@gmail.com>
2016-11-06 10:39:52 -0800

    Left Click to shoot bullet and kill MOONMEN

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -3,48 +3,24 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour {
 
-	public int startingHealth = 100;
-	public int currentHealth;
+
 	public float sinkSpeed = 2.5f;
 
-	ParticleSystem hitParticles;
-	CapsuleCollider capsulecollider;
+
 	bool isDead;
 	bool isSinking;
-
-	void Awake () {
-		hitParticles = GetComponentInChildren <ParticleSystem> ();
-		capsulecollider = GetComponent<CapsuleCollider> ();
-
-		currentHealth = startingHealth;
-	}
-
-
 
 	void Update () {
 		if (isSinking) {
 			transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
 		}
 	}
+		
 
-	public void TakeDamage (int amount, Vector3 hitPoint) {
-		if (isDead)
-			return;
-
-		currentHealth -= amount;
-
-		hitParticles.transform.position = hitPoint;
-		hitParticles.Play ();
-
-		if (currentHealth <= 0) {
-			Death ();
+	void OnTriggerEnter (Collider col) {
+		if (col.tag == "bullet") {
+			StartSinking ();
 		}
-	}
-
-	void Death () {
-		isDead = true;
-
-		capsulecollider.isTrigger = true;
 	}
 
 	public void StartSinking () {
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject bulletprefab;
+	public Transform bulletSpawn;
+	float speed = 20;
+
+	AudioSource gunAudio;
+
+
+	void Awake () {
+		gunAudio = GetComponent<AudioSource> ();
+
+	}
+
+	void Update () {
+		
+		if (Input.GetMouseButtonDown(0)) {
+			Fire ();
+			gunAudio.Play ();
+		} 
+	}
+
+	void Fire () {
+		GameObject Bullet = (GameObject)Instantiate (bulletprefab, bulletSpawn.position, bulletSpawn.rotation);
+		Bullet.GetComponent<Rigidbody> ().velocity = Bullet.transform.forward*speed;
+
+	}
+
+
+}
