833cffe9ac4b1a5f802728634c9352a21830e06b
Rosalind Chang <rosalindjchang@gmail.com>
2016-11-06 22:01:06 -0800

    Added Homebase so OOMMEN go and stay there when brought back by PLAYER from forest

--- Assets/Scripts/OommenController.cs
+++ Assets/Scripts/OommenController.cs
@@ -6,12 +6,14 @@ public class OommenController : MonoBehaviour {
 	Transform player;
 	Transform moonmen;
 	Transform oommen;
+	Transform home;
 	public GameObject oommenBod;
 
 	NavMeshAgent nav;
 	private float speed;
 	private float distBetweenMoonOom;
 	private float distFromPlayer;
+	private float distFromeHome;
 	private Animator oommenController;
 	Vector3 randomPoint; 
 	Transform getPos;
@@ -22,14 +24,15 @@ public class OommenController : MonoBehaviour {
 		randomPoint = Random.insideUnitSphere*200;
 		randomPoint.y = 0;
 		moonmen = GameObject.FindGameObjectWithTag ("moonmen").transform;
+		home = GameObject.FindGameObjectWithTag ("Home").transform;
 		oommen = transform;
 		nav = GetComponent<NavMeshAgent> ();
 
 	}
-
-
+		
 	void Update () {
 
+		distFromeHome = Vector3.Distance(oommen.position, home.position);
 		distFromPlayer = Vector3.Distance(player.position, oommen.position);
 		distBetweenMoonOom = Vector3.Distance (moonmen.position, oommen.position);
 
@@ -40,6 +43,10 @@ public class OommenController : MonoBehaviour {
 					nav.SetDestination (randomPoint);
 				} else {
 					nav.SetDestination (player.position);
+				}
+
+				if (distFromeHome < 20f) {
+					nav.SetDestination (home.position);
 				}
 
 		} else {
