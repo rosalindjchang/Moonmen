6f41160ba7cf858e4b36fd8413175d50ff3be826
Rosalind Chang <rosalindjchang@gmail.com>
2016-11-06 23:11:48 -0800

    added Start Scene

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+
+	public void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			SceneManager.LoadScene ("DOGGY");
+			SceneManager.UnloadScene ("START");
+
+		}
+	}
+
+}
